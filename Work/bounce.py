5267bd957107a3507577208585e5f41c5376b1c8
Thanos Dometios <thanos.dometios@clearspace.today>
2025-12-15 17:05:08 +0000

    Section1 exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100.0
+num_of_bounces = 10
+
+for i in range(1,num_of_bounces+1):
+    height = 3*height/5
+    print(i, round(height,4))
+    
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,34 @@
 # pcost.py
 #
 # Exercise 1.27
+# Exercise 1.30 - 1.33
+
+import sys
+
+def portfolio_cost(filename):
+    'Calculates the total cost of the portfolio added as an input file'
+    total_cost = 0.0
+    with open(filename) as file:
+        headers = next(file)
+        for line in file:
+            data = line.split(',')
+            try:
+                shares = int(data[1])
+            except ValueError:
+                print("Couldn't parse", line)
+
+            try:
+                price = float(data[2])
+            except ValueError:
+                print("Couldn't parse", line)
+            
+            total_cost += shares * price
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
